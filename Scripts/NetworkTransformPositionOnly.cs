cf363365e879cbdd7e65c4851a04cfa89f184a32
James Frowen <jamesfrowendev@gmail.com>
2020-12-10 13:06:26 +0000

    namespace

--- Scripts/NetworkTransformPositionOnly.cs
+++ Scripts/NetworkTransformPositionOnly.cs
@@ -1,7 +1,7 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-namespace Mirror
+namespace Mirror.PositionSyncing
 {
     public class NetworkTransformPositionOnly : NetworkBehaviour
     {
